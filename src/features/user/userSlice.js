9c0eaf77e02b1a077423414de431b9fe4a44cd28
Omer <codersixteen@outlook.com>
2023-12-04 00:19:35 -0500

    refactored theme toggle logic

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,19 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 
+const themes = {
+  dracula: "dracula",
+  winter: "winter",
+};
+
+const getThemeFromLocalStorage = () => {
+  const theme = localStorage.getItem("theme") || themes.winter;
+  document.documentElement.setAttribute("data-theme", theme);
+  return theme;
+};
+
 const initialState = {
   user: {
     username: "coding addict",
   },
-  theme: "dracula",
+  theme: "winter",
 };
 
 const userSlice = createSlice({
   name: "user",
-  initialState, 
-  reducers : {
+  initialState,
+  reducers: {
     loginUser: (state, action) => {
-      console.log("login")
-    }
-  }
-})
+      console.log("login");
+    },
+    logoutUser: (state) => {
+      console.log("logout");
+    },
+    toggleTheme: (state) => {
+      const { dracula, winter } = themes;
+      state.theme = state.theme === dracula ? winter : dracula;
+      document.documentElement.setAttribute("data-theme", state.theme);
+      localStorage.setItem("theme", state.theme);
+    },
+  },
+});
+
+export const { loginUser, logoutUser, toggleTheme } = userSlice.actions;
+export default userSlice.reducer;
